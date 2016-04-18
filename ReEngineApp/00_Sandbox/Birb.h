d2d3dd2566d6cc1f837f4317d1090c648fe5e431
Gregory Carrobis (RIT Student) <gtc3509@ad.rit.edu>
2016-04-18 19:46:28 -0400

    class includes

--- ReEngineApp/00_Sandbox/Birb.h
+++ ReEngineApp/00_Sandbox/Birb.h
@@ -1,4 +1,6 @@
 #pragma once
+#include "RE\ReEngAppClass.h"
+#include <SFML\Graphics.hpp>
 class Birb
 {
 public:
--- ReEngineApp/00_Sandbox/Player.h
+++ ReEngineApp/00_Sandbox/Player.h
@@ -1,7 +1,12 @@
 #pragma once
+#include "RE\ReEngAppClass.h"
+#include <SFML\Graphics.hpp>
 class Player
 {
 public:
+	vector3 bodyPosition;
+	vector3 legPosition;
+
 	Player();
 	~Player();
 };
